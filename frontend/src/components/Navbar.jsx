9825d9c7686a93b1812b414fea6b6c2e39f12717
Topiuhm <makinen.eeli03@gmail.com>
2026-03-06 17:06:22 +0200

    isAuthenticated and Navbar conditional rendering

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,31 @@
-const Navbar = () => {
+import { Link } from "react-router-dom"
+
+const Navbar = ({ isAuthenticated, setIsAuthenticated }) => {
+
+  const handleClick = () => {
+    localStorage.removeItem("user");
+    setIsAuthenticated(false);
+  }
+
   return (
     <nav className="navbar">
       <h1>Vehicle Rental</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/add-rental">Add Rental</a>
+        <Link to="/">Home</Link>
+
+        {isAuthenticated && (
+          <div>
+            <span>{JSON.parse(localStorage.getItem("user")).username}</span>
+            <button onClick={handleClick}>Logout</button>
+            <a href="/add-rental">Add Rental</a>
+          </div>
+        )}
+        {!isAuthenticated && (
+          <>
+            <Link to="/register"></Link>
+            <Link to="/login"></Link>
+          </>
+        )}
       </div>
     </nav>
   );
