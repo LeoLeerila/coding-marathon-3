b8a871dbcc3b1ed48061f11226411628be4a3653
Topiuhm <makinen.eeli03@gmail.com>
2026-03-06 14:32:58 +0200

    add necessary code to VehicleRentalPage and a link to VehicleRentalListing.jsx

--- frontend/src/components/VehicleRentalListing.jsx
+++ frontend/src/components/VehicleRentalListing.jsx
@@ -1,7 +1,7 @@
 const VehicleRentalListing = ({rentals}) => {
   return (
-    <div className="rental-preview">
-      <h2>Vehicle Model: {rentals.model}</h2>
+    <div className="rental-preview"><Link to={`/vehicleRentals/${rentals.id}`}>
+      <h2>{rentals.model}</h2></Link>
       <p>Category: {rentals.category}</p>
       <p>Daily Price: ${rentals.dailyPrice.toFixed(2)}</p>
       <p>Status: {rentals.availabilityStatus}</p>
